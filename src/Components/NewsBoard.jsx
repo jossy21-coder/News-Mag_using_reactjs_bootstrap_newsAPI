a15d587f2624c503a8cf8b875e301e01e0e001a3
Yosef Mihiret <yosefmihiret21@gmail.com>
2025-06-23 10:53:05 -0700

    news mag

---
+++ src/Components/NewsBoard.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+
+const NewsBoard = ({ category }) => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=5c87491eb4fe46a0963bec76a8428b9b`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setArticles(data.articles));
+  }, [category]);
+  return (
+    <div>
+      <h2 className="text-center">
+        Latest <span className="badge bg-danger">News</span>
+      </h2>
+
+      {articles.map((news, index) => {
+        return (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default NewsBoard;
